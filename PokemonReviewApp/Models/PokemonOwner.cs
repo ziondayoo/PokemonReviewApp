e0c2176258ecf75cdfc22dad03dedab5f2de9cfb
Adetunji Temidayo <adetunjitemidayo26@gmail.com>
2023-05-17 17:54:19 +0100

    Add project files.

---
+++ PokemonReviewApp/Models/PokemonOwner.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class PokemonOwner
+    {
+        public int OwnerId { get; set; }
+        public int PokemonId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
